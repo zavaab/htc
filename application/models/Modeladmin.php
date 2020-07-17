625d7e8ec2ad8a6a3bf997cd17856e8df1f0b693
zavaab <zadougani@gmail.com>
2020-07-17 11:59:18 +0430

    tbl_masters

--- application/models/Modeladmin.php
+++ application/models/Modeladmin.php
@@ -76,6 +76,7 @@ class Modeladmin extends CI_Model {
 	public function getmasters(){
 		$this->db->select('*');
 		$this->db->from('tbl_masters');
+		$this->db->order_by('tbl_masters.m_id', 'DESC');
 		$query = $this->db->get();
 		return $query->result();
 	}
